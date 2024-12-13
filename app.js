3d0f5ce9117f51f449295ebaca70114dff65b918
kazuhei takahashi <takahashikazuhei0218@gmail.com>
2024-12-13 15:10:32 +0900

    Update package.json2

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 静的ファイルの提供 (index.html, style.css, script.js など)
-app.use(express.static(path.join(__dirname)));
-
-// ルートアクセスで index.html を返す
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // サーバー起動
